01b082b76512335a204495a84498fe1c70546791
isabelrem <irt99@protonmail.com>
2023-11-09 17:44:36 +0000

    python script that 1) can read from and write to local mySQL database 2) can make new search records in the searches table and 3) can assign existing search record ids to new patients

--- sql/panelsearch_db.sql
+++ sql/panelsearch_db.sql
@@ -1,16 +1,19 @@
+-- DROP TABLE patients
+-- DROP TABLE searches
+
 -- Create panelsearch database if it does not already exist
 CREATE DATABASE IF NOT EXISTS panelsearch;
 
 -- Create table for patients IDs
 CREATE TABLE IF NOT EXISTS patients(
-	id int PRIMARY KEY,
+	id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     patient_id varchar(50),
     search_id int
 );
 
 -- Create table for search results
 CREATE TABLE IF NOT EXISTS searches(
-	id int, # patients.search_id
+	id int KEY AUTO_INCREMENT, # patients.search_id
     panel_id int,
     panel_name varchar(50),
     panel_version varchar(50),
@@ -20,7 +23,8 @@ CREATE TABLE IF NOT EXISTS searches(
     genes varchar(250),
     transcript varchar(50),
     genome_build varchar(50),
-    bed_file JSON
-);
+    bed_file varchar(50), #change to JSON when there is a JSON
+    UNIQUE (panel_id, panel_name, panel_version, GMS, gene_number, r_code, genes, transcript, genome_build, bed_file)
+    );
 
 
