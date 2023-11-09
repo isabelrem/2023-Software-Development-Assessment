3b1e7599976f49f39305df56e793e63375c2d1d3
Jessica <jessk999@yahoo.co.uk>
2023-11-09 12:09:57 +0000

    edited sql file

--- sql/panelsearch_db.sql
+++ sql/panelsearch_db.sql
@@ -5,18 +5,20 @@ CREATE DATABASE IF NOT EXISTS panelsearch;
 CREATE TABLE patients(
 	id int PRIMARY KEY,
     patient_id int,
-    r_code varchar(5)  # searches.r_code
+    first_name varchar(50),
+    surname varchar(50)
 );
 
 -- Create table for search results
 CREATE TABLE searches(
 	id int, 
+    patient_id int, # patients.patient_id
     panel_id int,
     panel_name varchar(50),
     panel_version varchar(50),
     GMS varchar(50),
     gene_number int,
-    r_code varchar(5),  # patients.r_code
+    r_code varchar(5),  
     genes varchar(250),
     transcript varchar(50),
     genome_build varchar(50),
