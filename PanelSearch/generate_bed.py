745746e5bad6965f2ba32a771606f12baac3f795
Egle Girdenyte <egleg@nbsvr484.northbristol.local>
2023-11-27 10:46:38 +0000

    Updated generate_bed.py with logging and main.py now outputs BED files to specific folder

--- PanelSearch/generate_bed.py
+++ PanelSearch/generate_bed.py
@@ -1,17 +1,33 @@
+"""
+The main function of this script processes gene panel data provided in JSON format, generating a BED file 
+suitable for genomic analysis. It reads command-line arguments for input data and output file name, ensuring 
+efficient and accurate conversion of panel data into the BED file format.
+"""
 import json
 import sys
+import logging
+from logging.handlers import RotatingFileHandler
+
+# Set up logging
+log_file = 'generate_bed.log'
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s %(message)s',
+    handlers=[
+        RotatingFileHandler(log_file, maxBytes=10000, backupCount=1),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
 
 def parse_panel_data(json_data):
     """
     Parses panel data from JSON and extracts gene information.
-
-    :param json_data: JSON string with panel data
-    :return: List of gene information as dictionaries
     """
     try:
         printed_panel = json.loads(json_data)
+        logging.debug("JSON data parsed successfully.")
     except json.JSONDecodeError as e:
-        print(f"Failed to decode JSON: {e}")
+        logging.error(f"Failed to decode JSON: {e}")
         sys.exit(1)
 
     genes = printed_panel.get('Genes', [])
@@ -23,7 +39,7 @@ def parse_panel_data(json_data):
             start, end = positions.split('-')
             bed = {
                 'chromosome': chromosome,
-                'start': start,
+                'start': str(int(start) - 1),  # Adjust start position for zero-based indexing
                 'end': end,
                 'gene': gene
             }
@@ -31,32 +47,36 @@ def parse_panel_data(json_data):
     
     return beds
 
-def write_bed_to_json(beds, filename='beds.json'):
+def write_bed_file(beds, filename):
     """
-    Writes the BED data to a JSON file.
-
-    :param beds: List of BED data
-    :param filename: File name to write the BED data
+    Writes the BED data to a file in BED format.
     """
-    bed_json = json.dumps(beds, indent=4)
-    with open(filename, 'w') as f:
-        f.write(bed_json)
+    try:
+        with open(filename, 'w') as file:
+            for bed in beds:
+                line = f"{bed['chromosome']}\t{bed['start']}\t{bed['end']}\t{bed['gene']}\n"
+                file.write(line)
+        logging.info(f"BED file written successfully to {filename}")
+    except Exception as e:
+        logging.error(f"Error writing BED file: {e}")
 
 def main():
     """
-    Main function to process panel data and generate BED file.
+    Main function to process panel data and generate a BED file.
     """
-    if len(sys.argv) < 2:
-        print("No input data provided.")
+    if len(sys.argv) < 3:
+        logging.warning("Insufficient arguments provided. Exiting.")
         sys.exit(1)
 
-    printed_panel_json = sys.argv[1]
-    beds = parse_panel_data(printed_panel_json)
-
-    # Optional: Write BED data to a file or handle as needed
-    # write_bed_to_json(beds)
-
-    print("BED file generated successfully.")
+    try:
+        printed_panel_json = sys.argv[1]
+        filename = sys.argv[2]
+        beds = parse_panel_data(printed_panel_json)
+        write_bed_file(beds, filename)
+        logging.info(f"BED file generated successfully as {filename}.")
+    except Exception as e:
+        logging.error(f"Unexpected error: {e}")
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
