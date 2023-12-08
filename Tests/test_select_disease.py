fdaf68fd872e05561ab11129ceb373334004b92e
Tom Livesey <thomas.livesey@postgrad.manchester.ac.uk>
2023-12-08 13:07:11 +0000

    Bugfixes to tests in test_select_disease.py to account for changes in the way the api url is handled in select_disease.py

--- Tests/test_select_disease.py
+++ Tests/test_select_disease.py
@@ -3,25 +3,7 @@ This is a test script for select_disease.py to ensure that find_match() function
 """
 
 # Import modules
-from PanelSearch.select_disease import find_match, get_clinical_indications, panel_url
-
-# Test the get_clinical_indications function from select_disease.py returns user-inputted disease
-def test_get_clinical_indications_works():
-    """
-    Get list of genetic diseases from PanelApp API that are GMS approved
-    :return: GMS approved genetic diseases as list
-    """
-    assert get_clinical_indications("https://panelapp.genomicsengland.co.uk/api/v1/panels/") is not False
-
-
-# Test the get_clinical_indications function from select_disease.py returns user-inputted disease
-def test_get_clinical_indications_errors():
-    """
-    Attempt to get list of genetic diseases from API that does not exist
-    :return: False
-    """
-    assert get_clinical_indications("www.aksljdfrni.com") is False  # Also works with https://www.aksljdfrni.com
-
+from PanelSearch.select_disease import find_match, get_clinical_indications
 
 # Test the find_match function from utils.select returns user-inputted disease
 def test_find_match_works(monkeypatch):
@@ -38,7 +20,7 @@ def test_find_match_works(monkeypatch):
 
     # go about using input() like you normally would:
     assert find_match("possible mitochondrial disorder - nuclear genes",
-                      get_clinical_indications(panel_url)) == 'possible mitochondrial disorder - nuclear genes'
+                      get_clinical_indications()) == 'possible mitochondrial disorder - nuclear genes'
 
 
 # Test the find_match function from utils.select returns error
@@ -55,6 +37,6 @@ def test_find_match_errors(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda _: "jibberjabber")
 
     # go about using input() like you normally would:
-    assert find_match("jibberjabber", get_clinical_indications(panel_url)) is False
+    assert find_match("jibberjabber", get_clinical_indications()) is False
 
 
