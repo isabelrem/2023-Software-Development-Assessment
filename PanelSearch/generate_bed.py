c8c11914691ac27ae01b333cf230c74dfc6495e1
Tom Livesey <thomas.livesey@postgrad.manchester.ac.uk>
2023-12-11 11:03:30 +0000

    Added user input to determine which type of coordiantes, transcript or genomic, are to be used in the bed file

--- PanelSearch/generate_bed.py
+++ PanelSearch/generate_bed.py
@@ -20,13 +20,16 @@ logging.basicConfig(
     ]
 )
 
-def parse_panel_data(json_data):
+def parse_panel_data(json_data, coord_type):
     """
     Parses panel data from JSON and extracts gene information.
 
     Args:
         json_data (str): JSON string containing panel data.
 
+        coord_type (str): string specifying whether the user wants
+                          genomic or transcript coordiantes in the
+                          produced bed file.
     Returns:
         list: List of dictionaries representing BED data.
     """
@@ -53,6 +56,12 @@ def parse_panel_data(json_data):
             bed['chromosome'] = chr
             bed['gene'] = symbol
             bed['transcript'] = transcript
+
+            if coord_type == 'trans':
+                bed['coordinates'] = 'transcript'
+            elif coord_type == 'gen':
+                bed['coordinates'] = 'genomic'
+
             bed['exons'] = []
 
             for exon in info[4]:
@@ -61,8 +70,12 @@ def parse_panel_data(json_data):
 
                 for k, v in exon.items():
                     exon_bed['exon_no'] = k
-                    exon_bed['start'] = v[0] # Do we need to adjust for zero-based indexing
-                    exon_bed['end'] = v[1]
+                    if coord_type == 'trans':
+                        exon_bed['start'] = v[0] # Do we need to adjust for zero-based indexing
+                        exon_bed['end'] = v[1]
+                    elif coord_type == 'gen':
+                        exon_bed['start'] = str(int(v[2]) - 1)
+                        exon_bed['end'] = str(int(v[3]) - 1)
                 
                 bed['exons'].append(exon_bed)
             
@@ -70,7 +83,7 @@ def parse_panel_data(json_data):
                 
     return beds
 
-def write_bed_file(beds, filename):
+def write_bed_file(beds, filename, coord_type):
     """
     Writes BED data to a file in both BED and JSON formats.
 
@@ -78,18 +91,27 @@ def write_bed_file(beds, filename):
         beds (list): List of BED data.
         filename (str): Filename for the output BED file.
 
+        coord_type(str): The type of coordinates, genomic or
+                         transcript, used to generate the bed
+                         file. 
     Returns:
         tuple: (bool, str) Indicates success status and message.
     """
     try:
         # Write to BED file
         with open(filename, 'w') as file:
-            headline = 'chromosome\tstart\tend\ttranscript\tgene\texon'
+            if coord_type == 'trans':
+                headline = 'chromosome\tstart\tend\ttranscript\tgene\texon'
+            elif coord_type == 'gen':
+                headline = 'chromosome\tstart\tend\tgene\texon'
             file.write(headline)
             
             for bed in beds:
                 for exon in bed['exons']:
-                    line = f"\n{bed['chromosome']}\t{exon['start']}\t{exon['end']}\t{bed['transcript']}\t{bed['gene']}\t{exon['exon_no']}"
+                    if coord_type == 'trans':
+                        line = f"\n{bed['chromosome']}\t{exon['start']}\t{exon['end']}\t{bed['transcript']}\t{bed['gene']}\t{exon['exon_no']}"
+                    elif coord_type == 'gen':
+                        line = f"\n{bed['chromosome']}\t{exon['start']}\t{exon['end']}\t{bed['gene']}\t{exon['exon_no']}"
                     file.write(line)
 
         # Write to JSON file
@@ -116,8 +138,19 @@ def main():
     try:
         printed_panel_json = sys.argv[1]
         filename = sys.argv[2]
-        beds = parse_panel_data(printed_panel_json)
-        write_bed_file(beds, filename)
+        
+        coord_type_no = ''
+        while coord_type_no != '1' and coord_type_no != '2':
+            coord_type_no = input('For exon coordinates on transcript, press \'1\'. For genomic coordinates, press \'2\'.')
+            if coord_type_no == '1':
+                coord_type = 'trans'
+            elif coord_type_no == '2':
+                coord_type = 'gen'
+            else:
+                print('Invalid input - try again')
+
+        beds = parse_panel_data(printed_panel_json, coord_type)
+        write_bed_file(beds, filename, coord_type)
     except Exception as e:
         logging.error(f"Unexpected error: {e}")
         sys.exit(1)
