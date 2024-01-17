816e69251bdcfe336bc6a57cc10b940735c1d41b
Jessica <jessk999@yahoo.co.uk>
2024-01-17 13:25:00 +0000

    SQL testing

--- PanelSearch/API_to_SQL_cloud.py
+++ PanelSearch/API_to_SQL_cloud.py
@@ -7,22 +7,29 @@ import pandas as pd
 RQ = PanelAppRequest()
 
 def PK_Parse_Data_to_SQL_cloud(pid, genome_build, PK):
-    response = RQ.pk_search(PK)
-    result = panelapp_search_parse(response.json(), genome_build)
+    """
+    Parses API JSON output and stores values as record in SQL database
+    """
+    try:
+        response = RQ.pk_search(PK)
+        result = panelapp_search_parse(response.json(), genome_build)
 
-    #print(result)
-    panel_id = result["Panel ID"]
-    panel_name = result["Panel Name"]
-    GMS = result["GMS Signed-off"]
-    gene_number = result["Gene Number"]
-    r_code = result["R Codes"]
-    panel_version = result["Version"] 
+        #print(result)
+        panel_id = result["Panel ID"]
+        panel_name = result["Panel Name"]
+        GMS = result["GMS Signed-off"]
+        gene_number = result["Gene Number"]
+        r_code = result["R Codes"]
+        panel_version = result["Version"] 
 
-    connect_cloud_db()
+        connect_cloud_db()
 
-    add_new_cloud_record(pid = pid,panel_id = panel_id,panel_name = panel_name, panel_version = panel_version,GMS= GMS,gene_number= gene_number,r_code= r_code , transcript = "a really good one",genome_build = genome_build,bed_file="placeholder")
+        add_new_cloud_record(pid = pid,panel_id = panel_id,panel_name = panel_name, panel_version = panel_version,GMS= GMS,gene_number= gene_number,r_code= r_code , transcript = "a really good one",genome_build = genome_build,bed_file="placeholder")
 
-    return "Function run"
+        return panel_id
+
+    except:
+        raise ValueError("Please enter correct panel ID, genome build, and panel name.")
 
 ### TESTING ###
 
